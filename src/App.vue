6c01a4a3b0e68445d46d5ca34650271bb90e32b4
Suin Kim <contact@suinkim.me>
2025-10-29 01:17:30 +0900

    feat: Card 컴포넌트 제작

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,42 @@
 <script setup lang="ts">
+import { ref } from 'vue'
+
+// components
 import ListHeader from './components/ListHeader.vue'
 import ListRow from './components/ListRow.vue'
 import ListRowTexts from './components/ListRowTexts.vue'
 import Button from './components/Button.vue'
 import Badge from './components/Badge.vue'
 import Spacing from './components/Spacing.vue'
+import Tab from './components/Tab.vue'
+import TabItem from './components/TabItem.vue'
+import Card from './components/Card.vue'
+
+const priority = ref('medium')
+
+const handlePriorityChange = (value: string) => {
+  priority.value = value
+}
 </script>
 
 <template>
   <ListHeader title="할 일 목록" />
+  <Card>
+    <template #header>
+      <Tab :model-value="priority" @update:model-value="handlePriorityChange">
+        <TabItem value="high">높음</TabItem>
+        <TabItem value="medium">보통</TabItem>
+        <TabItem value="low">낮음</TabItem>
+      </Tab>
+    </template>
+    <template #content>
+      <input type="date" />
+      <input type="text" placeholder="할 일을 입력하세요" />
+    </template>
+    <template #footer>
+      <Button>등록</Button>
+    </template>
+  </Card>
   <ListRow withPadding>
     <template #contents>
       <Badge status="warning" size="small">높음</Badge>
---
+++ src/components/Card.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="card">
+    <div class="card-header">
+      <slot name="header" />
+    </div>
+
+    <div class="card-content">
+      <slot name="content" />
+    </div>
+
+    <div class="card-footer">
+      <slot name="footer" />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.card {
+  width: 100%;
+  background-color: var(--color-background-soft);
+  border: 1px solid var(--color-border);
+  border-radius: 12px;
+  padding: 24px;
+  width: 100%;
+  margin: 0 auto;
+  color: var(--color-text);
+  box-sizing: border-box;
+  transition:
+    background-color 0.5s,
+    border-color 0.5s,
+    color 0.5s;
+}
+
+.card-header {
+  margin-bottom: 16px;
+  color: var(--color-heading);
+}
+
+.card-content {
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+}
+
+.card-footer {
+  margin-top: 24px;
+}
+</style>
