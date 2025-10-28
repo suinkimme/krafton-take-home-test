e2fde50181a933a3643ed5f79a762a402c2e156c
Suin Kim <contact@suinkim.me>
2025-10-29 00:14:14 +0900

    feat: ListRowTexts 컴포넌트 제작

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,27 @@
 <script setup lang="ts">
 import ListHeader from './components/ListHeader.vue'
+import ListRow from './components/ListRow.vue'
+import ListRowTexts from './components/ListRowTexts.vue'
+import Button from './components/Button.vue'
 </script>
 
 <template>
   <ListHeader title="할 일 목록" />
+  <ListRow withPadding>
+    <template #contents>
+      <ListRowTexts
+        top="할 일 1"
+        middle="할 일 1 내용할 일 1 내용할 일 1 내용할 일 1 내용할 일 1 내용할 일 1 내용할 일 1 내용할 일 1 내용"
+        bottom="2025년 10월 28일 오후 10시 00분"
+      />
+    </template>
+    <template #right>
+      <div style="display: flex; gap: 3px">
+        <Button size="small">수정</Button>
+        <Button theme="dark" size="small">삭제</Button>
+      </div>
+    </template>
+  </ListRow>
 </template>
 
 <style scoped></style>
